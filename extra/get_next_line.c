2686c8a694837da11be6dd000e73e1ebff9e46f0
Renato Francisco Pedrosa De Oliveira <rpedrosa@c1r5s5.42porto.com>
2024-11-28 15:22:19 +0000

    00

--- extra/get_next_line.c
+++ extra/get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "extra.h"
 
 static void	r_free(char **ptr)
 {
