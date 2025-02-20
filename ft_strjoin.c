b31b59759d48631ceaf64a5f50a9d68815c95d00
rfpoliveira <rfpoliveira18@gmail.com>
2025-02-20 12:33:31 +0000

    fixing strjoin when null char *

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -20,6 +20,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	i = 0;
 	y = 0;
+	if (!s1)
+		return ((char *)s2);
+	if (!s2)
+		return ((char *)s1);
 	joint = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (joint == NULL)
 		return ((char *)joint);
