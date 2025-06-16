5af10e4f66c5740fba0b6ca247f3c00c311bcb20
rfpoliveira <rfpoliveira18@gmail.com>
2025-06-16 11:47:37 +0100

    altering matrix_free

--- extra/matrix_free.c
+++ extra/matrix_free.c
@@ -6,7 +6,7 @@
 /*   By: rpedrosa <rpedrosa@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 11:21:32 by rpedrosa          #+#    #+#             */
-/*   Updated: 2025/06/11 15:51:16 by rpedrosa         ###   ########.fr       */
+/*   Updated: 2025/06/16 11:47:01 by rpedrosa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	matrix_free(char **mtx)
 	if (mtx == NULL)
 		return ;
 	while (mtx[i])
-		r_free(&mtx[i++]);
+		free(&mtx[i++]);
 	free(mtx);
 	mtx = NULL;
 }
